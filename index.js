94dc74c300adc888660310abfd83aa97a44d725d
Rasel Hossain <87034495+srrasel@users.noreply.github.com>
2022-04-13 07:44:32 +0600

    v1.

--- index.js
+++ index.js
@@ -10,17 +10,52 @@ app.use(cors())
 
 app.get('/:url',(req,res)=>{
     const dr= req.params
-    const ab = 'https://' +dr.url
+    const ab = 'https://' + dr.url
     const url = ab
-    
-    
+    getMetaData(url).then((data) => {
+        res.json(data)
+    })      
+})
+
+app.get('/:url/:suburl',(req,res)=>{
+    const dr= req.params
+    const subfurl= dr.suburl
+    const ab = 'https://' + dr.url +'/'+subfurl
+    const url = ab
+    getMetaData(url).then((data) => {
+        res.json(data)
+    })  
+})
+
+app.get('/:url/:suburl/:subturl',(req,res)=>{
+    const dr= req.params
+    const ab = 'https://' + dr.url +'/'+dr.suburl + '/' +dr.subturl
+    const url = ab
     getMetaData(url).then((data) => {
         res.json(data)
     })
       
-    
-    
+    app.get('/:url/:suburl/:subturl/:subfurl',(req,res)=>{
+        const dr= req.params
+        const ab =  'https://' + dr.url +'/'+dr.suburl + '/' +dr.subturl +'/'+dr.subfurl
+        const url = ab
+        getMetaData(url).then((data) => {
+            res.json(data)
+        }) 
+        
+    })
+
+    app.get('/:url/:suburl/:subturl/:subfurl/:submurl',(req,res)=>{
+        const dr= req.params
+        const ab =  'https://' + dr.url +'/'+dr.suburl + '/' +dr.subturl +'/'+dr.subfurl +'/'+dr.submurl
+        const url = ab
+        getMetaData(url).then((data) => {
+            res.json(data)
+        }) 
+        
+    })
     
 })
+
 app.listen(PORT,()=>console.log(`server running on PORT $(PORT)`))
 
